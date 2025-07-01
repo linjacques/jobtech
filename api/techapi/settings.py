3e3216a7a66cab0034ea11507a4e365dfb2ac1a7
JojoLePopo <joseph.destatguillot@gmail.com>
2025-07-01 19:03:43 +0200

    version sans mdp

--- api/techapi/settings.py
+++ api/techapi/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load environment variables from .env file
+load_dotenv(os.path.join(BASE_DIR.parent, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -79,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'JobTech',
         'USER': 'root',
-        'PASSWORD': '@Kikoo2304!+',
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '3306',
     }
