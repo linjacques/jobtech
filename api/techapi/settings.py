ffbc9697be79b55fecdb4d2c39f5ac637431f1e0
JojoLePopo <joseph.destatguillot@gmail.com>
2025-07-04 18:01:21 +0200

    push api

--- api/techapi/settings.py
+++ api/techapi/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'JobTech',
         'USER': 'root',
-        'PASSWORD': 'MDP,
+        'PASSWORD': '@Kikoo2304!+',
         'HOST': 'localhost',
         'PORT': '3306',
     }
